7134d165de9200fb6fcb3a9845ddde4441c4a1bf
Joel Angel <joelbaraka4325@gmail.com>
2022-06-07 12:00:38 +0300

    initialized the navigation properly

--- App.js
+++ App.js
@@ -1,14 +1,11 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Button } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
 import { useTailwind } from "tailwind-rn";
+import StackNavigator from "./StackNavigator";
 
 export default function App() {
-  const tailwind = useTailwind();
   return (
-    <View style={tailwind("flex-1 justify-center items-center")}>
-      <Text>Tinder Incoming!</Text>
-      <Button title="click" />
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <StackNavigator />
+    </NavigationContainer>
   );
 }
--- StackNavigator.js
+++ StackNavigator.js
@@ -1,16 +1,17 @@
 import { View, Text } from "react-native";
 import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "./screens/HomeScreen";
+import ChatScreen from "./screens/ChatScreen";
 
 const Stack = createNativeStackNavigator();
 
 const StackNavigator = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Chat" component={ChatScreen} />
+    </Stack.Navigator>
   );
 };
 
